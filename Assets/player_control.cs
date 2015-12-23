24583ab83c411120faebab1acfebe14ef63e67e9
suosong <suosong@sohu.com>
2015-12-23 18:26:42 +0800

    player 简单朝向对了

---
+++ Assets/player_control.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 euler = gameObject.transform.eulerAngles;
+	    if (Input.GetKey(KeyCode.W))
+        {
+            euler.z = 0;
+            gameObject.transform.eulerAngles = euler;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            euler.z = 180;
+            gameObject.transform.eulerAngles = euler;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            euler.z = 90;
+            gameObject.transform.eulerAngles = euler;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            euler.z = 270;
+            gameObject.transform.eulerAngles = euler;
+        }
+	}
+}
