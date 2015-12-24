45f3bf7d036f0a78391245f5b3c0475a143fe611
suosong <suosong@sohu.com>
2015-12-24 18:26:52 +0800

    简单的2d物理实验

--- Assets/player_control.cs
+++ Assets/player_control.cs
@@ -15,24 +15,35 @@ public class player_control : MonoBehaviour {
         {
             euler.z = 0;
             gameObject.transform.eulerAngles = euler;
+            gameObject.transform.Translate(0, 1 * Time.deltaTime, 0, Space.Self);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             euler.z = 180;
             gameObject.transform.eulerAngles = euler;
+            gameObject.transform.Translate(0, 1 * Time.deltaTime, 0, Space.Self);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             euler.z = 90;
             gameObject.transform.eulerAngles = euler;
+            gameObject.transform.Translate(0, 1 * Time.deltaTime, 0, Space.Self);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             euler.z = 270;
             gameObject.transform.eulerAngles = euler;
+            gameObject.transform.Translate(0, 1 * Time.deltaTime, 0, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            GameObject bullet = GameObject.Find("bullet");
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(gameObject.transform.up * 1, ForceMode2D.Impulse);
         }
 	}
 }
