579956f809232ce40ec077a50a018f2b259a16b9
suosong <suosong@sohu.com>
2015-12-29 18:47:23 +0800

    子弹出界检测

--- Assets/player_control.cs
+++ Assets/player_control.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class player_control : MonoBehaviour {
 
+    public GameObject bullet_pre;
+
 	// Use this for initialization
 	void Start () {
-	
+        bullet_pre = Resources.Load("prefabs/bullet") as GameObject;
 	}
 	
 	// Update is called once per frame
@@ -51,12 +53,12 @@ public class player_control : MonoBehaviour {
             gameObject.transform.Translate(0, 1 * Time.deltaTime, 0, Space.Self);
         }
 
-        if (Input.GetKey(KeyCode.J))
+        if (Input.GetKeyDown(KeyCode.J))
         {
-            GameObject bullet = GameObject.Find("bullet");
-            bullet.transform.position = gameObject.transform.position + gameObject.transform.up * 2;
+            GameObject bullet = GameObject.Instantiate(bullet_pre, gameObject.transform.position + gameObject.transform.up * 2,Quaternion.identity) as GameObject;
+            //bullet.transform.position = gameObject.transform.position + gameObject.transform.up * 2;
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-            rb.velocity = Vector2.zero;
+            //rb.velocity = Vector2.zero;
             rb.AddForce(gameObject.transform.up * 1, ForceMode2D.Impulse);
         }
 	}
